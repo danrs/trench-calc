c6004e33e20afb46dcfd8a86a67cc3a4c9faa79b
Dan Smith <5696173+danrs@users.noreply.github.com>
2024-01-17 17:13:19 +0800

    Create app.js

---
+++ app.js
@@ -0,0 +1,18 @@
+function update() {
+  let plus_dice = document.getElementById("plus_dice").value;
+  let minus_dice = document.getElementById("minus_dice").value;
+  let modifier = document.getElementById("modifier").value;
+  let odds = calculate_odds(plus_dice,minus_dice);
+  let modified_odds = modify(odds, modifier);
+
+  let str = JSON.stringify(action_odds(modified_odds), null, 4);
+  document.getElementById("action").innerText = str;
+  str = JSON.stringify(injury_odds(modified_odds), null, 4);
+  document.getElementById("injury").innerText = str;
+  str = JSON.stringify(modified_odds, null, 4);
+  document.getElementById("scores").innerText = str;
+}
+
+document.addEventListener("DOMContentLoaded", update, false);
+var calculate_button = document.getElementById('calculate_button');
+calculate_button.addEventListener("click", update, false);
